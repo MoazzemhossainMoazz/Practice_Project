3355e8bf465705309d79e0d8b7d769bb70742b47
MoazzemHossain <moazzemh222@gmail.com>
2024-07-26 11:30:59 +0600

    Added Javascript files

---
+++ JavaScript/Introduction.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>What Can JavaScript Do?</h2>
+
+<p id="demo">JavaScript can change HTML content.</p>
+
+<button type="button" onclick='document.getElementById("demo").innerHTML = "Hello JavaScript!"'>Click Me!</button>
+
+</body>
+
+<!-- Mirrored from www.w3schools.com/js/tryit.asp?filename=tryjs_intro_inner_html by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 14 Oct 2021 07:00:34 GMT -->
+</html>
